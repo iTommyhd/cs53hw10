4c67681a4b1b948f28fedee7459282290a94c235
JoseyStevens <josey1993@hotmail.com>
2013-11-23 20:50:48 -0600

    Main

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Main.h"
+
+using namespace std;
+
+int main()
+{
+  business MoesBar, ComicBookStore;
+  customer[NUMBER_CUSTOMERS];
+  for (int i = 0; i < NUMBER_CUSTOMERS;i++)
+  {
+    
+  }
+
+  return(0);
+}
