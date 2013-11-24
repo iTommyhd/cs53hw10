8d66c0531a2a4ef250b188d853b501475f3cf858
JoseyStevens <josey1993@hotmail.com>
2013-11-24 15:39:31 -0600

    Add features to main- reformatted business.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,16 +1,47 @@
 #include <iostream>
+#include<fstream>
 #include "Main.h"
+#include "Business.h"
 
 using namespace std;
 
 int main()
 {
   business MoesBar, ComicBookStore;
-  customer[NUMBER_CUSTOMERS];
+  customer ArrayCustomers[NUMBER_CUSTOMERS];
+  string TempName;
+  string TempInclination;
+
+  ifstream fin;
+  fin.open("customers.dat");
+  // Defines the customers in the array
   for (int i = 0; i < NUMBER_CUSTOMERS;i++)
   {
-    
+    //fin >> TempName;
+	getline(fin,TempName,',');
+	fin.ignore(30,' ');
+	getline(fin,TempInclination);
+	ArrayCustomers[i].change_name(TempName);
+	if(TempInclination == "1")
+	  ArrayCustomers[i].SetInclination(1);
+	else if (TempInclination =="-1")
+      ArrayCustomers[i].SetInclination(-1);
+	
   }
-
+  fin.close();
+  
+  // Enters the customers into their appropriate stores 
+  for(int i = 0; i < NUMBER_CUSTOMERS;i++)
+  {
+    if(ArrayCustomers[i].GetInclination() == 1)
+	{
+	  ComicBookStore.enter_customer(ArrayCustomers[i]);
+	}
+	if(ArrayCustomers[i].GetInclination() == -1)
+	{
+		MoesBar.enter_customer(ArrayCustomers[i]);
+	}
+  }
+  cin >> TempInclination;
   return(0);
 }
--- Main.h
+++ Main.h
@@ -1,3 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
 #include<fstream>
+using namespace std;
 
 const int NUMBER_CUSTOMERS = 20;
+const string CUSTOMER_FILE_NAME = "customers.dat";
+
+
+#endif
