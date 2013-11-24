406d9229e2fa781de675b3fce113d26989d0799d
iTommyhd <tommyd816@gmail.com>
2013-11-23 21:23:25 -0600

    Updated Formatting

--- Business.h
+++ Business.h
@@ -18,9 +18,9 @@ using namespace std;
 	    string business_name ; // name of business 
 	    float total_cash; //amount of cash in register 
 	    short int num_items;
-		short int num_customers;
-		string items[10]; // array containing items the business offers 
-		customer customers[10]; // customers in bussiness currently
+	    short int num_customers;
+	    string items[10]; // array containing items the business offers 
+	    customer customers[10]; // customers in bussiness currently
 
     public:
 		business():business_name(),total_cash(0),num_items(0),num_customers(0) 
@@ -33,7 +33,7 @@ using namespace std;
 		{
 			business_name = name;
 			total_cash = money_in_reg;
-		    num_customers = 0;
+		        num_customers = 0;
 			num_items = 7;
 			string stock [7] = { "item 1","item2","item3","item4","item5",
 		                   "item6","item7"};
@@ -43,7 +43,7 @@ using namespace std;
 		void print();
 
 		int get_numcust(){return num_customers;};
-	    void enter_customer(customer name_of_cust);
+	        void enter_customer(customer name_of_cust);
 		void make_a_sale();
 		customer leave_store(int index){return customers[index];}
 		void get_item(string item[],string stock[],short num_items);
